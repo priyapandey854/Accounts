c9abe71e72b137d4b1e2991c6e786b3ab076cdb8
priyapandey854 <priyadhanaval@gmail.com>
2025-10-20 08:18:30 +0530

    login

--- login/models.py
+++ login/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class User(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    username = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    form_type = models.CharField(max_length=20)  # 'signin' ya 'login'
+
+    def _str_(self):
+        return self.email
 
 
 # Create your models here.
--- login/views.py
+++ login/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render 
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import User
 
 def index(request):
-    return render(request,"index.html")
+    if request.method == "POST":
+        # Sign In form
+        if 'signin' in request.POST:
+            name = request.POST.get ('name')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            User.objects.create(name=name,email=email, password=password, form_type='signin')
+            return redirect('success')
+
+        # Login form
+        elif 'login' in request.POST:
+            name = request.POST.get('name')
+            username = request.POST.get('username')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            User.objects.create(name=name, username=username, email=email, password=password, form_type='login')
+            return redirect('success')
+
+    return render(request, 'index.html')
 
 
-# Create your views here.
-
+def success(request):
+    return render(request, 'success.html')
